52e0adc027b4658cd23e048e21ba5c958dfff5bc
dkds <dkds@users.noreply.github.com>
2026-03-23 22:14:32 +0530

    Add spring boot init setup

---
+++ oauth2-spring-boot-4/ui/auth-server/src/main/java/com/dkds/authcodetest1/Spring4AuthCodeTest1Application.java
@@ -0,0 +1,13 @@
+package com.dkds.authcodetest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4AuthCodeTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4AuthCodeTest1Application.class, args);
+	}
+
+}
